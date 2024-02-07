a75a68ceab0ae07999e9dfb6e24d12ff7e6860e1
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2024-02-08 00:12:51 +0100

    add user login logic

---
+++ migrations/Version20240207191510.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240207191510 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
+        // add a default user
+        $this->addSql('INSERT INTO `user` (email, roles, password) VALUES (\'leo.paillard@gmail.com\', \'["ROLE_ADMIN"]\', \'$2y$13$fJNgtutsCwxcHulgcHWmleD8MOG2gZIR3L2CHlogMM3/31P6s6xCG\')');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DELETE FROM `user` WHERE email = \'leo.paillard@gmail.com\'');
+    }
+}
