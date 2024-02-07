05d45b1e7bb8da5bedb41a64d2aa5358b5b6103b
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2024-02-08 00:16:12 +0100

    update js app route

--- public/assets/app/index.js
+++ public/assets/app/index.js
@@ -1,16 +1,22 @@
 import Theme from './Theme.js';
-import LetterCastleView  from "./castle/LetterCastleView.js";
-import NumberCastleView from "./castle/NumberCastleView.js";
+import LettersGameView  from "./game/LettersGameView.js";
+import NumbersGameView from "./game/NumbersGameView.js";
 import Router from './Router.js';
+import WordListView from "./game-setup/WordListView.js";
 
 let _theme = new Theme();
 let router = new Router();
 
-router.addRoute('/number/castle', () => {
-    let _numberCastleView = new NumberCastleView();
+router.addRoute('/number/game', () => {
+    let _numbersGameView = new NumbersGameView();
 });
-router.addRoute('/letter/castle', () => {
-    let _letterCastleView = new LetterCastleView();
+
+router.addRoute('/letter/game', () => {
+    let _lettersGameView = new LettersGameView();
+});
+
+router.addRoute('/flash/words', () => {
+    let _wordListView = new WordListView();
 });
 
 router.addRoute('/', () => {
